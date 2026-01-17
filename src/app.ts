7962ef166014cd2314151995b0427e85c4f23679
Your Name <your@email.com>
2026-01-17 14:15:01 -0600

    Allow inline event handlers in CSP

--- src/app.ts
+++ src/app.ts
@@ -21,6 +21,7 @@ app.use(helmet({
     directives: {
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrcAttr: ["'unsafe-inline'"],
       styleSrc: ["'self'", "'unsafe-inline'"],
       connectSrc: ["'self'"],
     }
